abc7c23b718ceeace74840d871efd8f45c3acd15
Gordon Wong <wg93128@gmail.com>
2014-03-05 00:44:23 +0800

    5/3/2014 ScheduleMatching update
    still buggy :(

--- ScheduleMatching.java
+++ ScheduleMatching.java
@@ -2,41 +2,42 @@ package comp3111project;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-
 //Example: 1/1/2014 12:45pm = DateAndTime(2014,Calendar.January,1,51)
 class DateAndTime{
 GregorianCalendar Date;
+static GregorianCalendar now = new GregorianCalendar();
 	int time_slot;
 	int weekday;
+	static DateAndTime Now = new DateAndTime(now.get(Calendar.YEAR),now.get(Calendar.MONTH),now.get(Calendar.DAY_OF_MONTH),now.get(Calendar.HOUR)*4+(now.get(Calendar.MINUTE)+1)/15);
 	
-	DateAndTime(int year,int month,int day_of_month,int interval){
+DateAndTime(int year,int month,int day_of_month,int interval){
 		Date = new GregorianCalendar();
 		Date.set(year, month, day_of_month);
 		time_slot=interval;
 		weekday=Date.get(Calendar.DAY_OF_WEEK);
 	}
-	
-	DateAndTime now()
+	String printthis()
 	{
-		GregorianCalendar now = new GregorianCalendar();
-		int day = now.get(Calendar.DAY_OF_MONTH);
-		int month = now.get(Calendar.MONTH);
-		int year = now.get(Calendar.YEAR);
-		int hour = now.get(Calendar.HOUR);
-		int min = now.get(Calendar.MINUTE);
-		int time_slot = hour*4+(min+1)/15;
-		DateAndTime result = new DateAndTime(year,month,day,time_slot);
-		return result;
+		
+		return " Year: "+Date.get(Calendar.YEAR)+" Month: "+(Date.get(Calendar.MONTH)+1)+" Day: "+Date.get(Calendar.DAY_OF_MONTH)+" Weekday: "+Date.get(Calendar.DAY_OF_WEEK)+" time: "+time_slot;
 	}
 	boolean before(DateAndTime date_and_time){
-		if (this.Date.before(date_and_time.Date))
+		if (Date.before(date_and_time.Date))
 			return true;
-		else if (this.time_slot<date_and_time.time_slot)
+		else if (time_slot<date_and_time.time_slot&&Date.equals(date_and_time.Date))
 			return true;
 		else
 			return false;
 	}
-	
+	boolean after(DateAndTime date_and_time){
+		if (Date.after(date_and_time.Date))
+			return true;
+		else if (time_slot>date_and_time.time_slot&&Date.equals(date_and_time.Date))
+			return true;
+		else
+			return false;
+	}
+
 	int NumOfDay(int month,int year){
 		if (month==GregorianCalendar.JANUARY||month==GregorianCalendar.MARCH||month==GregorianCalendar.MAY||month==GregorianCalendar.JULY||month==GregorianCalendar.AUGUST
 				||month==GregorianCalendar.OCTOBER||month==GregorianCalendar.DECEMBER)
@@ -88,11 +89,11 @@ return 30;
 	day += result.time_slot/96;
 	result.time_slot%=96;
 	day+=result.Date.get(Calendar.DAY_OF_MONTH);
-		            System.out.println("day(before loop): "+day);
+		          //  System.out.println("day(before loop): "+day);
 	while(true){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 		if (day>result.NumOfDay(result.Date.get(Calendar.MONTH), result.Date.get(Calendar.YEAR))){
 			day-=result.NumOfDay(result.Date.get(Calendar.MONTH), result.Date.get(Calendar.YEAR));
-		System.out.println("day(after loop): "+day);
+		//System.out.println("day(after loop): "+day);
 			result.Date.set(year,result.NextMonth(Calendar.MONTH), result.Date.get(Calendar.DAY_OF_MONTH));
 		if (result.Date.get(Calendar.MONTH)==Calendar.JANUARY)
 			year++;
@@ -103,16 +104,25 @@ return 30;
 	result.Date.set(year,result.Date.get(Calendar.MONTH), day);
 		return result;
 		}
-DateAndTime DateOfWeekday(RegularEventNode Weekday)
+
+DateAndTime FindDateOfWeekday(RegularEventNode Weekday)
 {
-	int count = Weekday.weekday-this.weekday;
+	int count = Weekday.begin.week_day-this.weekday;
 if (count<0)
 	count+=7;
 DateAndTime result = this.add(count*96);
-result.time_slot = Weekday.starting_interval;
+result.time_slot = Weekday.duration;
 return result;
 }
-	
+DateAndTime FindDateOfWeekday(int week_day)
+{
+	int count = week_day-this.weekday;
+if (count<0)
+	count+=7;
+DateAndTime result = this.add(count*96);
+result.time_slot = 0;
+return result;
+}
 }
 
 class User // object which stores user's info
@@ -128,18 +138,105 @@ User(String Name,int id,EventNode eventptr, RegularEventNode scheduleptr)
 	event_ptr=eventptr;
 	schedule_ptr=scheduleptr;
 	}
-/*DateAndTime FreeTimeSlot(DateAndTime time,int duration) // find a free time slot which is closest to 'time'
+
+void AddEvent(EventNode node)
+{
+	EventNode ptr=event_ptr;
+if (event_ptr==null)
+	event_ptr = node;
+else if (event_ptr.next==null)
+event_ptr.next=node;
+	else
+{	while(ptr.next!=null)
+		ptr=ptr.next;
+	ptr.next=node;
+}}
+void AddRegularEvent(RegularEventNode node)
+{
+	RegularEventNode ptr=schedule_ptr;
+	if(schedule_ptr==null)
+	{
+		schedule_ptr = node;
+	node.next = schedule_ptr;
+	System.out.println("schedule ptr is null");
+	}
+	else
+	{	while(true)
+	{
+		if (node.end.week_day<ptr.begin.week_day||
+				(node.end.week_day==ptr.begin.week_day&&node.end.time_slot<ptr.begin.time_slot))
+		{
+			System.out.println("node is put at the front");
+			node.next=ptr;
+		ptr=node;
+		break;
+		}
+		else if(ptr.next!=schedule_ptr&&ptr.next!=null)
+		{
+			System.out.println("ptr moves on");
+			ptr=ptr.next;
+		}else
+		{
+			System.out.println("node is put at the back");
+			node.next=schedule_ptr;
+			ptr.next=node;
+			break;
+		}
+	}
+	System.out.println("a node is put in the list");
+	}}
+void printregularevent()
+{
+	RegularEventNode ptr = schedule_ptr;
+while(ptr!=null)
+{	System.out.println("Regular event starts at: "+ptr.begin.printthis()+" event ends at: "+ptr.end.printthis());
+ptr=ptr.next;
+
+if(ptr.next==schedule_ptr||ptr.next==null)
+break;
+}
+System.out.println("***The end***");
+}
+
+void printevent()
+{
+EventNode ptr = event_ptr;
+while(ptr!=null)
+{	System.out.println("Event id: "+ptr.event_id+" event starts at: "+ptr.begin.printthis()+" event ends at: "+ptr.end.printthis());
+ptr=ptr.next;
+}
+System.out.println("***The end***");
+}
+DateAndTime FreeTimeSlot(DateAndTime time,int duration) // find a free time slot which is closest to 'time'
 {
 EventNode Event_ptr=event_ptr;
 RegularEventNode Schedule_ptr=schedule_ptr;
-DateAndTime result;
-if (Schedule_ptr.difference(Schedule_ptr.ptr)>=duration)
+DateAndTime result = DateAndTime.Now;
+boolean return_flag=false;
+while(true)
 {
-	
-} 
-
+if (Schedule_ptr.difference(Schedule_ptr.next)>=duration)
+{
+	result = result.FindDateOfWeekday(Schedule_ptr);
+while (Event_ptr!=null)
+	if (!Event_ptr.overlap(result))
+		Event_ptr=Event_ptr.next;
+	else
+	{
+		result = result.FindDateOfWeekday(Schedule_ptr);
+	}
 }
-*/
+else
+{
+	if (return_flag||Schedule_ptr.next==null)
+		break;
+	Schedule_ptr=Schedule_ptr.next;
+	if(Schedule_ptr.next==schedule_ptr)
+return_flag=true;
+}} 
+return result;
+}
+
 
 boolean IsFree(DateAndTime date_and_time)
 {
@@ -158,40 +255,79 @@ Guest(String Name,int id,EventNode eventptr, RegularEventNode scheduleptr)
 
 class RegularEventNode
 {
-int weekday,starting_interval,ending_interval,duration;
-RegularEventNode ptr;
+WeekdayAndTime begin,end;
+int duration;
+RegularEventNode next;
 
-RegularEventNode(int Weekday,int Starting_interval,int Duration)
+RegularEventNode(int Weekday,int Begin,int Duration)
 {
-	weekday=Weekday;
-	starting_interval=Starting_interval;
+	begin = new WeekdayAndTime(Weekday,Begin);
 	duration=Duration;
-	ending_interval=starting_interval+duration;
-	}
+	end = this.add(duration);
+next=null;	
+}
+
+WeekdayAndTime add(int time)
+{
+	WeekdayAndTime result;// = new WeekdayAndTime(begin.week_day,duration);
+	int day=0;
+	time+=begin.time_slot;	
+if (time>95)
+{
+	day+=time/96;
+	time%=96;
+}
+
+//System.out.println("day: "+day+" time: "+time);
+result = new WeekdayAndTime(begin.week_day+day,time);
+return result;
+}
 int difference (RegularEventNode node)
 {
-int day_difference = (node.weekday-this.weekday);
+int day_difference = (node.begin.week_day-this.end.week_day);
 if (day_difference<0)
 	day_difference+=7;
-	int interval = Math.abs(this.duration-node.duration)+day_difference*96;
+	int interval = Math.abs(duration-node.duration)+day_difference*96;
 return interval;
 }
+
 }
+class WeekdayAndTime
+{
+	int week_day,time_slot;
+	WeekdayAndTime(int weekday,int timeslot)
+	{
+		week_day=weekday;
+		time_slot = timeslot;
+	}
+	String printthis()
+	{
+		return "weekday:" +week_day+" time slot: "+time_slot;
+		}
+	}
 class EventNode
 {
 	User eventholder;	
 	int event_id,duration;	
-	DateAndTime starting_date_and_time;
-	DateAndTime ending_date_and_time;
-EventNode next=null;	
+	DateAndTime begin;
+	DateAndTime end;
+EventNode next;	
 	EventNode(User a,int b,DateAndTime d,int e)
 {
 	eventholder = a ;
 	event_id = b ;
-	starting_date_and_time = d ;	
+	begin = d ;	
 	duration=e;
-//ending_date_and_time=d.add(e);
+	end = d.add(e);
+	next=null;
 }
+	boolean overlap(DateAndTime date_and_time)
+	{
+		if (end.before(date_and_time)||begin.after(date_and_time))
+	return false;
+		else
+			return true;
+	}
 }
 
 class Event extends EventNode // store extra information of an event
@@ -222,13 +358,39 @@ Event(String a,int b,User c,DateAndTime d,int e)
 		*/
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
-	//	GregorianCalendar a =new GregorianCalendar(2014,10,12);
-		//DateAndTime b = new DateAndTime(2001,Calendar.FEBRUARY,27,8);
-	//DateAndTime c=b.add(386);
-	//System.out.println("year: "+c.Date.get(Calendar.YEAR)+" month: "+c.Date.get(Calendar.MONTH)+" day: "+c.Date.get(Calendar.DAY_OF_MONTH)+" interval: "+c.time_slot);
-		RegularEventNode a= new RegularEventNode(Calendar.MONDAY,48,4),b=new RegularEventNode(Calendar.SUNDAY,52,4);
-System.out.print(a.difference(b));
+User Gordon = new User("Gordon",01,null,null);
+DateAndTime a = new DateAndTime(2014,Calendar.MARCH,5,76), b  = new DateAndTime(2014,Calendar.MARCH,7,72);
+
+EventNode event1 = new EventNode(Gordon,01,a,8), event2 = new EventNode(Gordon,02,b,4);
+RegularEventNode event3 = new RegularEventNode(Calendar.MONDAY,0,3);
+RegularEventNode event4 = new RegularEventNode(Calendar.TUESDAY,0,4);
+RegularEventNode event5 = new RegularEventNode(Calendar.WEDNESDAY,0,5);
+RegularEventNode event6 = new RegularEventNode(Calendar.THURSDAY,0,6);
+RegularEventNode event7 = new RegularEventNode(Calendar.FRIDAY,0,7);
+RegularEventNode event8 = new RegularEventNode(Calendar.SATURDAY,0,8);
+RegularEventNode event9 = new RegularEventNode(Calendar.SUNDAY,0,9);
+
+Gordon.AddEvent(event1);
+Gordon.AddEvent(event2);
+System.out.println("add event done");
+//Gordon.AddRegularEvent(event8);
+Gordon.AddRegularEvent(event9);
+Gordon.AddRegularEvent(event3);
+Gordon.AddRegularEvent(event4);
+Gordon.AddRegularEvent(event5);
+Gordon.AddRegularEvent(event6);
+Gordon.AddRegularEvent(event7);
+
+
+System.out.println("add regular event done");
+Gordon.printevent();
+Gordon.printregularevent();
+RegularEventNode ptr = Gordon.schedule_ptr.next;
+while (ptr!=null&&ptr!=Gordon.schedule_ptr)
+{System.out.println("+1");
+ptr=ptr.next;
+}
+	Gordon.event_ptr.begin.printthis();
 	}
 
 	}
