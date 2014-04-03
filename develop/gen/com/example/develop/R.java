92e6a6db7a701fdf1848ef9c8b703d3f12bea027
wilmer234 <wilmer13093@gmail.com>
2014-04-03 23:54:40 +0800

    Events android example added
    An example showing how to parse json object and present it.

--- develop/gen/com/example/develop/R.java
+++ develop/gen/com/example/develop/R.java
@@ -24,21 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int RL=0x7f080000;
-        public static final int action_settings=0x7f08000e;
-        public static final int b_confirm=0x7f080005;
-        public static final int b_event=0x7f080008;
-        public static final int b_login=0x7f080007;
-        public static final int b_msgbox=0x7f080009;
-        public static final int b_polling=0x7f08000a;
-        public static final int b_reg=0x7f080006;
-        public static final int b_schedule=0x7f08000c;
-        public static final int b_slist=0x7f08000b;
-        public static final int debugbox=0x7f08000d;
-        public static final int et_id=0x7f080003;
-        public static final int et_pw=0x7f080004;
-        public static final int t_id=0x7f080002;
-        public static final int t_pw=0x7f080001;
+        public static final int RL=0x7f080001;
+        public static final int action_settings=0x7f08000f;
+        public static final int b_confirm=0x7f080006;
+        public static final int b_event=0x7f080009;
+        public static final int b_login=0x7f080008;
+        public static final int b_msgbox=0x7f08000a;
+        public static final int b_polling=0x7f08000b;
+        public static final int b_reg=0x7f080007;
+        public static final int b_schedule=0x7f08000d;
+        public static final int b_slist=0x7f08000c;
+        public static final int debugbox=0x7f08000e;
+        public static final int et_id=0x7f080004;
+        public static final int et_pw=0x7f080005;
+        public static final int events=0x7f080000;
+        public static final int t_id=0x7f080003;
+        public static final int t_pw=0x7f080002;
     }
     public static final class layout {
         public static final int activity_event=0x7f030000;
--- develop/src/com/example/develop/Event.java
+++ develop/src/com/example/develop/Event.java
@@ -1,15 +1,37 @@
 package com.example.develop;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Event extends Activity {
-
+	
+    private ProgressDialog pDialog;
+    private static final String LOGIN_URL = "http://124.244.60.23/weu/event.php";
+    JSONParser jsonParser = new JSONParser();
+    TextView Events;
+    String temp="";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event);
+		Events = (TextView)findViewById(R.id.events);
+		new ShowEvents().execute();
 	}
 
 	@Override
@@ -18,5 +40,58 @@ public class Event extends Activity {
 		getMenuInflater().inflate(R.menu.event, menu);
 		return true;
 	}
+	
+	class ShowEvents extends AsyncTask<String, String, String> {
+
+		boolean failure = false;
+		
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Event.this);
+            pDialog.setMessage("Loading...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+
+        }
+		
+		@Override
+		protected String doInBackground(String... args) {
+			// TODO Auto-generated method stub
+            List<String> Events_joined = new ArrayList<String>();
+            
+            try {
+            	
+            	List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", "admin"));	//put the name here, it will return the events, here i use admin
+
+                JSONArray jArray = jsonParser.makeHttpRequest(LOGIN_URL, params);
+
+               for(int i = 0; i <jArray.length();i++ ) {
+            	                   JSONObject json = jArray.getJSONObject(i);
+
+            	                   Events_joined.add(json.getString("event_name"));	//an example to show you how to get data from json object
+               }
+
+               for(int i = 0; i<Events_joined.size();i++) {
+            	   temp = temp+Events_joined.get(i).toString()+"\n";
+               }
+               
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+ 
+            return null;
+			
+		}
+
+        protected void onPostExecute(String file_url) {
+            pDialog.dismiss();
+            Events.setText(temp);
+        }
+		
+	}
+
 
 }
