964782d7c918c9094ecaefc4bbe952478bd714b3
kwlishirley <kwlishirley@gmail.com>
2014-04-19 19:52:14 +0800

    recommit

--- WeUnion/WeUnion/bin/Login-instrumented-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Yip\workspace\WeUnion\bin\Login-instrumented-unaligned.apk : \
-C:\Users\Yip\workspace\WeUnion\bin\Login.ap_ \
-C:\Users\Yip\workspace\WeUnion\bin\classes.dex \
-C:\Users\Yip\workspace\WeUnion\libs\android-support-v4.jar \
-C:\Users\Yip\Desktop\adt-bundle-windows-x86_64-20131030\sdk\tools\lib\emma_device.jar \
